a5815a79b63ae137798b6f867dabc69104a522bf
Putita <putita.techapat@mail.kmutt.ac.th>
2021-02-16 00:02:30 +0700

    favorite

---
+++ fav.js
@@ -0,0 +1,4 @@
+char = prompt('What is your favorite charactor?')
+color = prompt('What is the main color of '+ char)
+console.log(char + ' is your favorite charactor')
+console.log(char + ' is ' + color )
