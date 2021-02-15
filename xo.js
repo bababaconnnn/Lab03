cc14c81f67e7331ba5e07ab3e66ab09bdfa10d58
Putita <putita.techapat@mail.kmutt.ac.th>
2021-02-15 23:12:12 +0700

    first

---
+++ xo.js
@@ -0,0 +1,7 @@
+function roundX() {
+    const image = document.querySelector("img");
+    image.src="https://banner2.cleanpng.com/20180423/rye/kisspng-tic-tac-toe-computer-icons-clip-art-5add84df649c12.1755804215244669114121.jpg";
+}
+
+const image = document.querySelector("img");
+image.addEventListener("click",roundX);
